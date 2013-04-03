fa77627a5114276abc50582e1c9ec295dafc41a1
arnaldorusso <arnaldorusso@gmail.com>
2013-04-03 18:08:38 -0300

    modern template

--- README.md
+++
@@ -1,4 +0,0 @@
-PyPAM
-=====
-
-Read light curves and photosynthetic yield values, obtained from PyhtoPAM equipment. Plot values of light and photosynthesis parameters. alpha, Ik, ETR(max), Beta and 
--- setup.py
+++ setup.py
@@ -2,41 +2,37 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
 version = '0.1'
 
 install_requires = [
-    'rpy',
-    'numpy',
-    'matplotlib'
+    # List your project dependencies here.
+    # For more details, see:
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
 ]
+
 
 setup(name='PyPAM',
     version=version,
-    description="Reads light curves and photosynthetic yield values, obtained from PyhtoPAM equipment. Plot values of light and photosynthesis parameters. alpha, Ik, ETR(max) and Beta",
+    description="Read light curves and photosynthetic yield values, obtained from PyhtoPAM equipment. Plot values of light and photosynthesis parameters. alpha, Ik, ETR(max) and Beta.",
     long_description=README + '\n\n' + NEWS,
     classifiers=[
-        'Development Status :: 1 - Planning'# {'2': 'Pre-Alpha', '3': 'Alpha'}
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Python Software Foundation License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Topic :: Scientific/Engineering',
+      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     ],
-    keywords='music oceanography data',
+    keywords='Pulse Amplitude Modulate, Photosynthesis, Fluorescence, Electron Transport Rate (ETR), Primary Production, Phytoplankton',
     author='Arnaldo Russo',
     author_email='arnaldorusso@gmail.com',
-    url='https://github.com/arnaldorusso/PyPAM',
-    download_url='https://github.com/arnaldorusso/PyPAM/archive/master.tar.gz',
+    url='http://ciclotux.blogspot.com',
     license='PSF',
     packages=find_packages('src'),
-    package_dir = {'': 'src'},
-    include_package_data=True,
+    package_dir = {'': 'src'},include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
-    platforms='any',
+    entry_points={
+        'console_scripts':
+            ['PyPAM=pypam:main']
+    }
 )

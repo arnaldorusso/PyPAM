28091766c6c4f5a9a2756b70c679727c99d9149a
arnaldorusso <arnaldorusso@gmail.com>
2013-10-07 10:31:03 +0200

    developing

--- README.rst
+++
@@ -1,5 +0,0 @@
-PyPAM - Pulse Amplitude Modulate with Python
-============================================
-
-Read light curves and photosynthetic yield values, obtained from PyhtoPAM equipment.
-Extract values as: alpha, Ik, ETR(max) and Beta.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.rst')).read()
+README = open(os.path.join(here, 'README.md')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
@@ -27,8 +27,8 @@ setup(name='PyPAM',
     author_email='arnaldorusso@gmail.com',
     url='http://ciclotux.blogspot.com',
     license='PSF',
-    packages=find_packages('src'),
-    package_dir = {'': 'src'},include_package_data=True,
+    packages=find_packages('pypam'),
+    package_dir = {'': 'pypam'},include_package_data=True,
     zip_safe=False,
     install_requires=install_requires,
     entry_points={

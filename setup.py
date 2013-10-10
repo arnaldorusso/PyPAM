c799036b884bc88f3c7b539508c67d6a3b418642
arnaldorusso <arnaldorusso@gmail.com>
2013-10-10 16:12:56 +0200

    developing proccess - Permissions and write directories on setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,50 @@
-from setuptools import setup, find_packages
-import sys, os
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
-NEWS = open(os.path.join(here, 'NEWS.txt')).read()
-
-
-version = '0.1'
-
-install_requires = [
-    # List your project dependencies here.
-    # For more details, see:
-    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
-]
+import os
+import sys
 
 
-setup(name='PyPAM',
-    version=version,
-    description="Read light curves and photosynthetic yield values, obtained from PyhtoPAM equipment. Plot values of light and photosynthesis parameters. alpha, Ik, ETR(max) and Beta.",
-    long_description=README + '\n\n' + NEWS,
-    classifiers=[
-      # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    ],
-    keywords='Pulse Amplitude Modulate, Photosynthesis, Fluorescence, Electron Transport Rate (ETR), Primary Production, Phytoplankton',
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+readme = open('README.md').read()
+history = open('HISTORY.md').read().replace('.. :changelog:', '')
+
+setup(
+    name='PyPAM',
+    version='0.1.0',
+    description='PyPAM - Read light curves and photosynthetic yield values, obtained from PyhtoPAM equipment. Extract values as: alpha, Ik, ETR(max) and Beta.',
+    long_description=readme + '\n\n' + history,
     author='Arnaldo Russo',
     author_email='arnaldorusso@gmail.com',
-    url='http://ciclotux.blogspot.com',
-    license='PSF',
-    packages=find_packages('pypam'),
-    package_dir = {'': 'pypam'},include_package_data=True,
+    url='https://github.com/arnaldorusso/PyPAM',
+    packages=[
+        'PyPAM',
+    ],
+    package_dir={'PyPAM': 'PyPAM'},
+    include_package_data=True,
+    install_requires=[
+    ],
+    license="PSF",
     zip_safe=False,
-    install_requires=install_requires,
-    #entry_points={
-    #    'console_scripts':
-    #        ['PyPAM=pypam:main']
-    #}
+    keywords='PyPAM, Pulse Amplitude Modulate, Photosynthesis, Fluorescence, Electron Transport Rate (ETR), Primary Production, Phytoplankton',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Python Software Foundation License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    test_suite='tests',
 )

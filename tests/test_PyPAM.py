1f69c78cdf67e9929e2384146c36ace484b7d0cb
arnaldorusso <arnaldorusso@gmail.com>
2013-10-10 16:07:44 +0200

    developing proccess - Permissions and write directories on setup.py

---
+++ tests/test_PyPAM.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_PyPAM
+----------------------------------
+
+Tests for `PyPAM` module.
+"""
+
+import unittest
+
+from PyPAM import PyPAM
+
+
+class TestPadrao(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

c3e58d0a0d617e08eba9bd1486e595395817dcb4
arnaldorusso <arnaldorusso@gmail.com>
2013-10-14 12:14:05 +0200

    returns optimized values and curve parameters (alpha, Ik, etr_max)

--- PyPAM/etr_models.py
+++ PyPAM/etr_models.py
@@ -9,29 +9,55 @@ from rpy import r
 
 def platt(light,etr):
     '''
+    Adjust a curve of best fitt, following the Platt model.
 
+    Parameters
+    ----------
+    light : arr
+        Generally PAR values. Where Photosynthetic Active Radiance
+        interfer on Primary Production. 'light' is this parameter.
+
+    etr : arr
+        Eletron Transference Rate, by means relative ETR, obtained from
+        Rapid Light Curves.
+
+    Returns
+    -------
+    opts : arr
+        Values optimized
+
+    params : arr
+        Parameters of curve ('alpha', 'Ik', 'rETR_max')
+
+    See Also
+    --------
+    T. Platt, C.L. Gallegos and W.G. Harrison, 1980. Photoinibition of photosynthesis in natural
+        assemblages of marine phytoplankton
+        
     '''
-    values = zip(light,etr)
     opts = []
-    for vals in values:
-        r.assign("x",vals[0])
-        r.assign("y",vals[1])
-        min_platt = r("""
-        platt<- function(params){
-            alpha<-params[1]
-            Beta<- params[2]
-            Ps<- params[3]
-            return( sum( (y-Ps*(1-exp(-alpha*x/Ps))*exp(-Beta*x/Ps))^2))
-        } """)
-        min_adp = r("""
-        min_ad<-function(params){ 
-            alpha<-params[1]
-            Beta<-params[2]
-            Ps<-params[3]
-            return( ( (Ps*(1-exp(-alpha*x/Ps)) *exp(-Beta*x/Ps)) ))
-        }""")
-        r('etr_sim<-optim(par=c(0.4, 1.5 , 80),fn=platt)')
-        opts.append(r('min_ad(par = etr_sim$par)'))
+    pars = []
 
-    return opts
+    r.assign("x", light)
+    r.assign("y", etr)
+    min_platt = r("""
+    platt<- function(params){
+        alpha<-params[1]
+        Beta<- params[2]
+        Ps<- params[3]
+        return( sum( (y-Ps*(1-exp(-alpha*x/Ps))*exp(-Beta*x/Ps))^2))
+    } """)
+    min_adp = r("""
+    min_ad<-function(params){ 
+        alpha<-params[1]
+        Beta<-params[2]
+        Ps<-params[3]
+        return( ( (Ps*(1-exp(-alpha*x/Ps)) *exp(-Beta*x/Ps)) ))
+    }""")
+    r('etr_sim<-optim(par=c(0.4, 1.5 , 80),fn=platt)')
+
+    opts = np.append(opts, r('min_ad(par = etr_sim$par)'))
+    pars = r('etr_sim$par')
+
+    return opts, pars
 
